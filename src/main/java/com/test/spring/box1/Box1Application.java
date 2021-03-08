b5419375431462845b67890953f877523a22a1c9
Gaurav Verma <gaverma7@in.ibm.com>
2021-03-08 15:06:32 +0530

    first commit

---
+++ src/main/java/com/test/spring/box1/Box1Application.java
@@ -0,0 +1,13 @@
+package com.test.spring.box1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Box1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Box1Application.class, args);
+	}
+
+}
